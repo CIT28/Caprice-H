fdeb43f1479cd004e68fdf6404b6f0e1ac975081
Avabeast17 <classicstudy17@gmail.com>
2025-08-23 18:32:52 -0700

    I am 85% sure I got it

--- querying/w3-q.sql
+++ querying/w3-q.sql
@@ -12,3 +12,24 @@
 -- run with:
 -- sqlite3 longlist.db < w3-q.sql
 
+-- ===== Q1: Books under 200 pages =====
+.output '| cat >> pow-w3.txt'
+.print 'Q1: Books under 200 pages (title, author, pages)'
+
+SELECT title, author, pages
+FROM longlist
+WHERE pages < 200
+ORDER BY pages ASC;
+
+.output stdout
+
+-- ===== Q2: How many books under 200 pages? =====
+.output '| cat >> pow-w3.txt'
+.print 'Q2: COUNT of books under 200 pages'
+
+SELECT COUNT(*) AS under_200_count
+FROM longlist
+WHERE pages < 200;
+
+.output stdout
+
