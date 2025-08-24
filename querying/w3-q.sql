a773e3e294d595a32217c1f2cfac8c4ef3883545
Avabeast17 <classicstudy17@gmail.com>
2025-08-23 18:29:33 -0700

    setup done

---
+++ querying/w3-q.sql
@@ -0,0 +1,14 @@
+-- Week 3 code/workflow challenge POW
+.mode box
+
+-- append output to new Week 3 POW file
+.output '| cat >> pow-w3.txt'
+
+.print 'w3 setup check: first run OK'
+-- no query yet
+
+.output stdout
+
+-- run with:
+-- sqlite3 longlist.db < w3-q.sql
+
