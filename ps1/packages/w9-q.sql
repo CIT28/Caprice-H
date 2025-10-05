3e21aa4d6846d4763a61ed442e1ba368a9e89525
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-05 16:17:33 +0000

    final step

--- ps1/packages/w9-q.sql
+++ ps1/packages/w9-q.sql
@@ -5,13 +5,17 @@
 -- send output to pow-p1.txt
 .output '| cat >> pow-a.txt'
 .print 'Lost Letter JOIN query'
+.print 'final step'
 
 
-SELECT * FROM "scans"
-JOIN "packages" ON "packages"."id" = "scans"."package_id"
-JOIN "addresses" ON "addresses"."id" = "packages"."to_address_id"
-    AND "addresses"."address" LIKE '%2 Finn%';
-
-
-
+SELECT "action", "timestamp" FROM "scans" s
+JOIN "packages" p ON p."id" = s."package_id"
+JOIN "addresses" ta ON ta."id" = p."to_address_id"
+    AND ta."address" LIKE '%2 Finn%'
+JOIN "addresses" fa ON fa."id" = p."from_address_id"
+    AND fa."address" LIKE '%900 Somerville Avenue%';
+-- ta for identifier to address
+-- fa for identifier from address
+-- p for identifier from address
+-- s for identifier from address
 -- sqlite3 packages.db < w9-q.sql
