e5842ce53fad4e2599658fac622dd34798c6ff62
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-13 03:00:15 +0000

    query 1

---
+++ ps1/dese/README.md
@@ -0,0 +1,12 @@
+# DESE Query 1 thru 6
+
+## Query 1
+
+```SELECT "name", "city", "type"
+FROM "schools";
+```
+
+## Query 2
+
+
+## Query 2
--- ps1/dese/dese-q.sql
+++ ps1/dese/dese-q.sql
@@ -1,37 +1,46 @@
 .mode box
-.output ' | cat >> pow-ds.txt'
+.output pow-dese.txt
 --queries below here
 .print '<----->'
-.print 'DESE tables and schema'
-.tables
-.schema
+-- .print 'DESE tables and schema'
+-- .tables
+-- .schema
 
 
-.print 'First 10 rows: districts'
-SELECT * FROM "districts" LIMIT 10;
+-- .print 'First 10 rows: districts'
+-- SELECT * FROM "districts" LIMIT 10;
 
-.print 'First 10 rows: graduation_rates'
-SELECT * FROM "graduation_rates" LIMIT 10;
+-- .print 'First 10 rows: graduation_rates'
+-- SELECT * FROM "graduation_rates" LIMIT 10;
 
-.print 'First 10 rows: staff_evaluations'
-SELECT * FROM "staff_evaluations" LIMIT 10;
+-- .print 'First 10 rows: staff_evaluations'
+-- SELECT * FROM "staff_evaluations" LIMIT 10;
 
-.print 'First 10 rows: expenditures'
-SELECT * FROM "expenditures" LIMIT 10;
+-- .print 'First 10 rows: expenditures'
+-- SELECT * FROM "expenditures" LIMIT 10;
 
-.print 'First 10 rows: schools'
-SELECT * FROM "schools" LIMIT 10;
+-- .print 'First 10 rows: schools'
+-- SELECT * FROM "schools" LIMIT 10;
 
-.print '<----->'
+-- .print '<----->'
 
 
 
 .print 'query 1'
 .print 'find the names and cities of all public schools in Massachusetts'
 
+
+SELECT "name", "city", "type"
+FROM "schools"
+WHERE "type" LIKE '%Public School%';
+
 .print 'Count for query 1'
 .print 'Count results'
 
-
+SELECT COUNT(*) AS 'Pub School Count' FROM (
+    SELECT "name", "city", "type"
+    FROM "schools"
+    WHERE "type" = '%Public School%'
+);
 .output stdout
 -- bash command sqlite3 dese.db < dese-q.sql
