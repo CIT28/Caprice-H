5f1bcb296aac0654986f159cef03c79ecbde7259
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-18 20:59:32 +0000

    2. query, poe, pow complete

--- ps1/moneyball/mball-q.sql
+++ ps1/moneyball/mball-q.sql
@@ -3,17 +3,46 @@
 .mode box
 -- final query only
 
-.print '<--Query 1 avg salary by year-->'
+.print '<--Query 2 Cal Ripken jr salary by year-->'
 SELECT
-  "year" AS "year",
-  ROUND(AVG("salary"), 2) AS "average salary"
-FROM "salaries"
-GROUP BY "year"
-ORDER BY "year" DESC;
+  s."year"   AS "year",
+  s."salary" AS "salary"
+FROM "salaries" s
+JOIN "players" p ON p."id" = s."player_id"
+WHERE p."first_name" = 'Cal'
+  AND p."last_name"  = 'Ripken'
+  AND p."birth_year" = 1960
+ORDER BY s."year" DESC;
 
-.print '<--Query 1 count-->'
-SELECT COUNT(DISTINCT "year") AS "P(1)_count"
-FROM "salaries";
+.print '<--Query 2 count-->'
+SELECT COUNT(*) FROM (
+  SELECT s."year"
+  FROM "salaries" s
+  JOIN "players" p ON p."id" = s."player_id"
+  WHERE p."first_name" = 'Cal'
+    AND p."last_name"  = 'Ripken'
+    AND p."birth_year" = 1960
+);
+
+
+
+
+
+
+
+
+
+-- .print '<--Query 1 avg salary by year-->'
+-- SELECT
+--   "year" AS "year",
+--   ROUND(AVG("salary"), 2) AS "average salary"
+-- FROM "salaries"
+-- GROUP BY "year"
+-- ORDER BY "year" DESC;
+
+-- .print '<--Query 1 count-->'
+-- SELECT COUNT(DISTINCT "year") AS "P(1)_count"
+-- FROM "salaries";
 
 .output stdout
 -- bash: sqlite3 moneyball.db < mball-q.sql
