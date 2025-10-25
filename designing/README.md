086e80e9510c54a0b62989f0e3baa2cc9a9d034f
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-25 19:38:08 +0000

    schema for different longlist.db

--- designing/README.md
+++ designing/README.md
@@ -1,3 +1,4 @@
 # Week 12 Attendance
-- 
+- Answer how can we reference the different veriosn of the longlist.db
+without copying it to the desinging folder?
 -
--- designing/w12-q.sql
+++ designing/w12-q.sql
@@ -3,10 +3,11 @@
 
 .output '| cat >> pow-a.txt'
 .print '<------------>'
-.print 'Schema for single table version of longlist.db'
-
+-- .print 'Schema for single table version of longlist.db'
+.print 'Schema for multiple table version of longlist.db'
+.schema
 
 
 
 .output stdout
--- bash: sqlite3 ??.db < w12-q.sql
+-- bash: sqlite3 ../relating/longlist.db < w12-q.sql
