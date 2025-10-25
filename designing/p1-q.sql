6932d72ba3a12e643cb00b323e8e8162ca9a000f
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-25 22:44:31 +0000

    CREATE TABLE

---
+++ designing/p1-q.sql
@@ -0,0 +1,29 @@
+-- starting the pow workflow
+.output pow-p1.txt
+.headers on
+.mode box
+
+-- .print '<-- create riders -->'
+-- CREATE TABLE IF NOT EXISTS "riders" (
+--   "id",
+--   "name"
+-- );
+
+-- .print '<-- stations -->'
+-- CREATE TABLE IF NOT EXISTS "stations" (
+--   "id",
+--   "name",
+--   "line"
+-- );
+
+-- .print '<-- create visits -->'
+-- CREATE TABLE IF NOT EXISTS "visits" (
+--   "rider_id",
+--   "station_id"
+-- );
+
+.print '<-- schema -->'
+.schema
+
+.output stdout
+-- bash: sqlite3 designing/mbta.db < designing/p1-q.sql
