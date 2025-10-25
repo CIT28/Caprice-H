83fb42e3f2b24173c870f1d65d6bccdd2b8c665e
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-10-25 18:58:29 +0000

    w12 setup done

---
+++ designing/w12-q.sql
@@ -0,0 +1,12 @@
+
+.mode box
+
+.output '| cat >> pow-a.txt'
+.print '<------------>'
+.print 'Schema for single table version of longlist.db'
+
+
+
+
+.output stdout
+-- bash: sqlite3 ??.db < w12-q.sql
