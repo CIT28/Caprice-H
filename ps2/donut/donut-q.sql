17be34a10fdec1b7b0151d3405778da333dbe127
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-11-08 19:45:39 +0000

    donuts setup

--- ps2/donut/donut-q.sql
+++ ps2/donut/donut-q.sql
@@ -1,14 +1,20 @@
 .output pow-d.txt
 .mode box
+-- Donuts and ingredients
+-- Requirements covered
+--keeping track of the ingredients, including name, unit, and price per unit.
+--Keep track of donuts, including name, glutenfree, and price.
+--Be able to look up which ingredients belong to each donut.
 
-.print 'donuts schema'
+.print 'creating donuts and ingredients tables from schema.sql'
 .read schema.sql
 
 .print 'verify tables'
 .tables
 
-.print 'full schema'
-.schema
-
+.print 'showing schema for donuts and ingredients'
+.schema ingredients
+.schema donuts
+.schema donut_ingredients
 
 -- bash: sqlite3 donuts.db < donut-q.sql
--- ps2/donut/schema.sql
+++ ps2/donut/schema.sql
@@ -1,1 +1,36 @@
+-- Donuts and Ingredients schema
+
+DROP TABLE IF EXISTS "donut_ingredients";
+DROP TABLE IF EXISTS "donuts";
+DROP TABLE IF EXISTS "ingredients";
+
+-- Ingredients:
+-- - ingredient name flour, cocoa, sugar
+-- - Track price per unit and the unit type (lb, g)
+CREATE TABLE "ingredients" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT NOT NULL UNIQUE,
+    "unit" TEXT NOT NULL,
+    "price_per_unit" NUMERIC NOT NULL CHECK ("price_per_unit" > 0)
+);
+
+-- Donuts
+-- name, gluten-free flag, and price per donut
+CREATE TABLE "donuts" (
+    "id" INTEGER PRIMARY KEY,
+    "name" TEXT NOT NULL UNIQUE,
+    -- 0 = not glutenfree, 1 = glutenfree
+    "gluten_free" INTEGER NOT NULL CHECK ("gluten_free" IN (0, 1)),
+    "price" NUMERIC NOT NULL CHECK ("price" > 0)
+);
+
+-- Link the table so we can see which ingredients are used in each donut
+-- be able to look up the ingredients for each of the donuts
+CREATE TABLE "donut_ingredients" (
+    "id" INTEGER PRIMARY KEY,
+    "donut_id" INTEGER NOT NULL,
+    "ingredient_id" INTEGER NOT NULL,
+    FOREIGN KEY ("donut_id") REFERENCES "donuts"("id"),
+    FOREIGN KEY ("ingredient_id") REFERENCES "ingredients"("id")
+);
 
