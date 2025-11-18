45c34ce0720b6191c0ba1764fb3ba9907e724b24
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-11-18 03:05:00 +0000

    explored constraints: unique and not null

--- writing/p1-q.sql
+++ writing/p1-q.sql
@@ -15,19 +15,36 @@
 
 
 
-.print 'INSERT rows'
+-- .print 'INSERT rows'
 
-.print 'profusion of flowers — 56.257'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('Profusion of Flowers','56.257','1956-04-12');
+-- .print 'profusion of flowers — 56.257'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('Profusion of Flowers','56.257','1956-04-12');
 
-.print 'farmers working at dawn 11.6152'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('Farmers working at dawn','11.6152','1911-08-03');
+-- .print 'farmers working at dawn 11.6152'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('Farmers working at dawn','11.6152','1911-08-03');
 
-.print 'spring outing 14.76'
+-- .print 'spring outing 14.76'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('Spring Outing','14.76','1914-01-08');
+
+
+.print 'Inserting "Spring Outing" WITHOUT an id. Expect: a new row with the next id.'
 INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('Spring Outing','14.76','1914-01-08');
+VALUES ('Spring Outing','14.76-NEW','1914-01-08');
+
+.print 'trying to insert a dupe accession_number ("14.76")'
+INSERT INTO "collections" ("title","accession_number","acquired")
+VALUES ('Duplicate test','14.76','1914-01-08');
+
+.print 'try to insert with title = NULL'
+INSERT INTO "collections" ("title","accession_number","acquired")
+VALUES (NULL,'XX.000','2000-01-01');
+
+.print 'insert with accession_number = NULL'
+INSERT INTO "collections" ("title","accession_number","acquired")
+VALUES ('No accession',NULL,'2000-01-01');
 
 
 .print 'Show schema'
