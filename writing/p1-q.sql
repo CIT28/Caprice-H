ee3487f97733f268c57cd0ac2bdee5d208b92576
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-11-18 03:14:21 +0000

    inserted multiple rows with one statement

--- writing/p1-q.sql
+++ writing/p1-q.sql
@@ -30,22 +30,31 @@
 -- VALUES ('Spring Outing','14.76','1914-01-08');
 
 
-.print 'Inserting "Spring Outing" WITHOUT an id. Expect: a new row with the next id.'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('Spring Outing','14.76-NEW','1914-01-08');
+-- .print 'Inserting "Spring Outing" WITHOUT an id. Expect: a new row with the next id.'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('Spring Outing','14.76-NEW','1914-01-08');
 
-.print 'trying to insert a dupe accession_number ("14.76")'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('Duplicate test','14.76','1914-01-08');
+-- .print 'trying to insert a dupe accession_number ("14.76")'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('Duplicate test','14.76','1914-01-08');
 
-.print 'try to insert with title = NULL'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES (NULL,'XX.000','2000-01-01');
+-- .print 'try to insert with title = NULL'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES (NULL,'XX.000','2000-01-01');
 
-.print 'insert with accession_number = NULL'
-INSERT INTO "collections" ("title","accession_number","acquired")
-VALUES ('No accession',NULL,'2000-01-01');
+-- .print 'insert with accession_number = NULL'
+-- INSERT INTO "collections" ("title","accession_number","acquired")
+-- VALUES ('No accession',NULL,'2000-01-01');
 
+.print 'INSERT multiple rows with one statement'
+INSERT INTO "collections" ("title","accession_number","acquired")
+VALUES
+  ('Pro flowers','21.100','1921-06-01'),
+  ('farmers sky','21.101','1921-06-02'),
+  ('butterfuly and peonies','21.102','1921-06-03');
+
+.print 'lets see if it worked'
+SELECT * FROM "collections";
 
 .print 'Show schema'
 .schema collections
