7c3babbd4e3cf745cf859f69fcab6869ecf840c6
CapricelovesData <97200735+Avabeast17@users.noreply.github.com>
2025-12-02 03:15:12 +0000

    query 2 done update trigger

--- ps3/dont-panic/w17-q.sql
+++ ps3/dont-panic/w17-q.sql
@@ -5,19 +5,36 @@
 
 --  fire the INSERT trigger
 
-.print 'query 1INSERT trigger demo'
+-- .print 'query 1INSERT trigger demo'
 
-.print 'show users count and last 5 user_logs'
-SELECT COUNT(*) AS user_count FROM users;
+-- .print 'show users count and last 5 user_logs'
+-- SELECT COUNT(*) AS user_count FROM users;
+-- SELECT id, type, old_username, new_username, old_password, new_password
+-- FROM user_logs ORDER BY id DESC LIMIT 5;
+
+-- .print 'insert a new throwaway user to trigger INSERT log'
+-- INSERT INTO users (username, password)
+-- VALUES ('probe_user', 'abc123');
+
+-- .print 'confirm user exists and log row was written'
+-- SELECT id, username FROM users WHERE username='probe_user';
+-- SELECT id, type, old_username, new_username, old_password, new_password
+-- FROM user_logs ORDER BY id DESC LIMIT 5;
+
+
+.print 'update the trigger demo'
+.print 'show probe_user current password n last 5 logs'
+SELECT username, password FROM users WHERE username='probe_user';
 SELECT id, type, old_username, new_username, old_password, new_password
 FROM user_logs ORDER BY id DESC LIMIT 5;
 
-.print 'insert a new throwaway user to trigger INSERT log'
-INSERT INTO users (username, password)
-VALUES ('probe_user', 'abc123');
+.print 'update probe_user password to trigger update log'
+UPDATE users
+SET password = 'newpass!'
+WHERE username = 'probe_user';
 
-.print 'confirm user exists and log row was written'
-SELECT id, username FROM users WHERE username='probe_user';
+.print 'verify row changed and a new update log was written'
+SELECT username, password FROM users WHERE username='probe_user';
 SELECT id, type, old_username, new_username, old_password, new_password
 FROM user_logs ORDER BY id DESC LIMIT 5;
 
